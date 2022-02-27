e69a8235cc9f40d1ed9526436d48e934d7e73271
illupovero <davideluporini1996@gmail.com>
2022-02-27 23:23:19 +0100

    snack 2/3 done

---
+++ snack-3.php
@@ -0,0 +1,30 @@
+<!-- Creare un array con 15 numeri casuali,
+ tenendo conto che l’array non dovrà contenere 
+ lo stesso numero più di una volta. -->
+<?php
+ $numbers = [];
+
+while (count($numbers) < 15) {
+   $number = rand(1, 100);
+   if (!in_array($number, $numbers)) {
+      $numbers[] = $number;
+   }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>snack 3</title>
+</head>
+<body>
+    <h1>Elenco numeri</h1>
+    <?php for ($i = 0 ; $i < count($numbers); $i++) { ?>
+    <p><?= $numbers[$i] ?></p>
+    <?php } ?>
+</body>
+</html>
---
+++ snack4.php
@@ -0,0 +1,6 @@
+<!-- # SNACK 4
+ Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi in base ai punti: ogni punto un nuovo paragrafo.
+
+ DIVIDERE stringa in base ad un delmitatore, crea un array
+ explode(delimitatore, stringa); -->
+
